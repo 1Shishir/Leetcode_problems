61565cc278052003e9d5596b606b029e86b6d6fa
Hasanur Rahman Shishir <90507563+1Shishir@users.noreply.github.com>
2024-01-13 13:05:48 +0600

    Create Length_of_Last_Word.java

---
+++ Length_of_Last_Word.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/length-of-last-word/
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int n = s.length();
+        int right = n - 1;
+        int count = 0;
+
+        while(right >= 0 && s.charAt(right) == ' ')
+            right--;
+
+        while(right >= 0 && s.charAt(right--) != ' ')
+            count++;
+        
+        return count;
+    }
+}
