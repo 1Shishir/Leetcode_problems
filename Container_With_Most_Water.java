30031b0d05f79efbffd4420b751af863724c2b88
Hasanur Rahman Shishir <90507563+1Shishir@users.noreply.github.com>
2024-01-22 16:17:13 +0600

    Update

---
+++ Container_With_Most_Water.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/container-with-most-water
+
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length, l = 0, r = n-1, maxArea = 0;
+        while(l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r-l));
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxArea;
+    }
+}
