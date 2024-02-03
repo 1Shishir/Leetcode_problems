cbc2f26d9f37cca2377c3e4c8abe56ba1a351161
Hasanur Rahman Shishir <90507563+1Shishir@users.noreply.github.com>
2024-02-03 18:30:34 +0600

    Update

---
+++ Isomorphic_Strings.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/isomorphic-strings/submissions/
+
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+        if(s.length()!=t.length())
+            return false;
+        for(int idx=0;idx<s.length();idx++)
+        {
+            if(map1[s.charAt(idx)]!=map2[t.charAt(idx)])
+                return false;
+            map1[s.charAt(idx)]=idx+1;
+            map2[t.charAt(idx)]=idx+1;
+        }
+        return true;
+    }
+}
